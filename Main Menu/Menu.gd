eba202e7936f15ea3d16f8606472bd051a46a1cd
brandjtc <103974491+brandjtc@users.noreply.github.com>
2022-11-13 19:52:06 -0500

    Updated Main Menu

--- Main Menu/Menu.gd
+++ Main Menu/Menu.gd
@@ -1,16 +1,28 @@
 extends Control
 
 func _ready():
-	$Menu/VBoxContainer/Start.grab_focus()
-
+	$Fade/Fade_Animation.play("Intro Fade")
 
 func _on_Start_pressed():
-	get_tree().change_scene("res://Test Node.tscn")
+	$Fade.visible=true;
+	$Fade/Fade_Animation.play("Fade_Start")
 
 
 func _on_Options_pressed():
-	pass # Replace with function body.
-
+	$Fade.visible=true;
+	$Fade/Fade_Animation.play("Fade_Option")
 
 func _on_Quit_pressed():
-	get_tree().quit();
+	$Fade.visible=true;
+	$Fade/Fade_Animation.play("Fade_Quit")
+
+
+func _on_Fade_Animation_animation_finished(anim_name):
+	if (anim_name=="Fade_Intro"):
+		$Fade.visible=false
+	if (anim_name=="Fade_Start"):
+		get_tree().change_scene("res://Test Node.tscn")
+	elif (anim_name=="Fade_Option"):
+		get_tree().change_scene("res://Main Menu/Options Menu.tscn")
+	else:
+		get_tree().quit();
