fa144f15747ce2d2754985def932acefeb7575cc
brandjtc <103974491+brandjtc@users.noreply.github.com>
2022-11-16 21:52:52 -0500

    Delete Level Construction Tools/Teleporter directory

--- Level Construction Tools/Teleporter/TpScript.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite
-
-func _ready():
-	pass
-	
-
-
-func _on_Area2D_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	get_tree().change_scene("res://Main Menu/Options Menu.tscn")
