b78d86b061944c3e56820738729b54a7480a17c3
brandjtc <103974491+brandjtc@users.noreply.github.com>
2022-11-29 00:42:01 -0500

    Delete Main Menu directory

--- Main Menu/Spin Gremlin 2.gd
+++
@@ -1,6 +0,0 @@
-extends ParallaxLayer
-export(float) var speed=30
-
-func _process(delta)->void:
-	self.motion_offset.x-=speed*delta
-	self.rotate(delta*speed/140*-1)
--- Main Menu/diagonalparalax.gd
+++
@@ -1,6 +0,0 @@
-extends ParallaxLayer
-export(float) var speed=60
-
-func _process(delta)->void:
-
-	self.rotate(delta*speed/140*-1)
